f64d80cc0d2a852e4d3a38a35b74dce2e8ae49c5
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2020-03-13 10:47:46 -0400

    Adds addPoint
    Method for adding new points.
    Also have example chart displaying.

--- CTATChart.js
+++ CTATChart.js
@@ -18,11 +18,37 @@ export default class CTATChart extends CTAT.Component.Base.Tutorable {
 
   _init() {
     this.setInitialized(true);
-    var graph_area = this.getDivWrap();
+    const graph_area = this.getDivWrap();
     this.setComponent(graph_area);
     this.addComponentReference(this, graph_area);
-    this._chart = new Chart(graph_area);
+    this._chart = new Chart(graph_area, {
+      type: 'scatter',
+      data: {
+        datasets: [{
+          label: 'Dataset',
+          data: [
+            { x: 0, y: 0 },
+            { x: 5, y: 5 }
+          ]
+        }]
+      }
+    });
+  }
+
+  addPoint(x, y) {
+    this.chart.data.datasets.forEach((dataset) => {
+      dataset.data.push({x: x, y: y});
+    });
+    this.chart.update();
   }
 }
 
 window.CTAT.ComponentRegistry.addComponentType('CTATChart', CTATChart);
+
+// Needed if charts are to be printed.
+// https://www.chartjs.org/docs/latest/general/responsive.html#important-note
+window.addEventListener("beforeprint", () => {
+  for (let id in Chart.instances) {
+    Chart.instances[id].resize();
+  }
+});
