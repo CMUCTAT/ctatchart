3fadea45c508f0f6c026097b8b984b83469f40e2
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2020-10-14 09:09:18 -0400

    Updates to webpack 5.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/CTATChart.js',
+  target: ["web", "es5"],
   output: {
     filename: 'CTATChart.js',
     path: path.resolve(__dirname, 'lib'),
